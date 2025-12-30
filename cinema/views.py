57d51f44efc1e0bf13bde62a92a4cb3042c0b5a1
Ann Riabykina <anny.r20.1994@gmail.com>
2025-12-30 14:43:25 +0200

    Py api serializers

--- cinema/views.py
+++ cinema/views.py
@@ -1,1 +1,53 @@
-# write views here
+from rest_framework import viewsets
+from .models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+)
+from .serializers import (
+    GenreSerializer, ActorSerializer, CinemaHallSerializer,
+    MovieListSerializer, MovieDetailReadSerializer,
+    MovieDetailWriteSerializer, MovieSessionListSerializer,
+    MovieSessionDetailReadSerializer, MovieSessionDetailWriteSerializer
+)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        elif self.action in ["create", "update", "partial_update"]:
+            return MovieDetailWriteSerializer
+        else:
+            return MovieDetailReadSerializer
+
+
+class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieSessionListSerializer
+        elif self.action in ["create", "update", "partial_update"]:
+            return MovieSessionDetailWriteSerializer
+        else:
+            return MovieSessionDetailReadSerializer
